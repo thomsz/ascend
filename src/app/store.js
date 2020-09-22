1d9e409c3b196394da6567c0a65f43ae74e6eb9b
Tom Segev <tom@golaender.com>
2020-09-22 14:58:54 +0300

    Add persisted state from localStorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mainReducer from './reducers';
 
-export default configureStore({
+export const loadState = () => {
+	try {
+		const serializedState = localStorage.getItem('state');
+		if (serializedState === null) {
+			return undefined;
+		}
+		return JSON.parse(serializedState);
+	} catch (err) {
+		return undefined;
+	}
+};
+
+export const saveState = (state) => {
+	try {
+		const serializedState = JSON.stringify(state);
+		localStorage.setItem('state', serializedState);
+	} catch {
+		// ignore write errors
+	}
+};
+
+const persistedState = loadState();
+
+const store = configureStore({
 	reducer: mainReducer,
+	preloadedState: persistedState,
 });
+
+store.subscribe(() => {
+	saveState(store.getState());
+	console.log(persistedState);
+});
+
+export default store;
